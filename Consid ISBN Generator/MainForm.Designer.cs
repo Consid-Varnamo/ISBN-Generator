02e121fc75f237eaa7a72426ed34189cb54d504f
chrhei <christer.heinback@consid.se>
2015-03-10 16:25:27 +0100

    Added project to Git

---
+++ Consid ISBN Generator/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Consid.ISBN.Generator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtISBNList = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtISBNList
+            // 
+            this.txtISBNList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtISBNList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtISBNList.Location = new System.Drawing.Point(0, 0);
+            this.txtISBNList.Multiline = true;
+            this.txtISBNList.Name = "txtISBNList";
+            this.txtISBNList.Size = new System.Drawing.Size(308, 234);
+            this.txtISBNList.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnClose.Location = new System.Drawing.Point(0, 211);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(308, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "&Stäng";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnClose;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 234);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtISBNList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Consid ISBN Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtISBNList;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Consid ISBN Generator/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Consid.ISBN.Generator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string s in Common.GenerateISBNNumberSeries())
+                sb.AppendLine(s);
+
+            txtISBNList.Text = sb.ToString();
+            this.Focus();
+            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
